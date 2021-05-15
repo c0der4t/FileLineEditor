9b10e6eac8a8892ca4a257881cb7c473b2f783a1
Monte Ekron <monte@ekronds.co.za>
2021-05-15 20:28:20 +0200

    Initial Commit

---
+++ Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileLineEditor
+{
+    public partial class frmMain : Form
+    {
+        public string TargetFilename;
+        public string Add_Before_Line;
+        public string Add_After_Line;
+
+        string LicFileLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\1969.lic");
+
+
+        //Progress Variables
+        public int TotalStepsRequired;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            dlgBrowseTarget.ShowDialog();
+            edtTargetFile.Text = dlgBrowseTarget.FileName;
+        }
+
+        private void worker_FileProcessor_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressbar.Maximum = TotalStepsRequired;
+
+            progressbar.Value = e.ProgressPercentage;
+            lblProgressMenu.Text = e.UserState.ToString();
+        }
+
+        private void worker_FileProcessor_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var random = new Random();
+            string ProcessID = Convert.ToString(random.Next(1, 999999));
+
+            //Calculate the amount of work and get progress reporting values.
+            worker_FileProcessor.ReportProgress(0, "Preparing...");
+            TotalStepsRequired = 0;
+
+            //Count Lines in File
+            var lineCount = 0;
+            using (var reader = File.OpenText(TargetFilename))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    lineCount++;
+                }
+            }
+
+            TotalStepsRequired += lineCount;
+            worker_FileProcessor.ReportProgress(0, "Preparing...");
+
+
+            //Make a backup of the file
+            if (chckbxMakeBackup.Checked)
+            {
+                TotalStepsRequired += 2;
+
+                worker_FileProcessor.ReportProgress(progressbar.Value, "Making Backup of File");
+                if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\")))
+                {
+                    Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\"));
+                }
+
+                File.Copy(TargetFilename, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\" + ProcessID + ".txt"),true);
+                worker_FileProcessor.ReportProgress(progressbar.Value + 1, "Processing File");
+
+            }
+
+            //Loop through original file and write changed lines to the temp file
+            try
+            {
+                StreamWriter TempFile = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\" + ProcessID + ".tmp"));
+                using (StreamReader OrgFile = new StreamReader(TargetFilename))
+                {
+                    string line;
+
+                    while ((line = OrgFile.ReadLine()) != null)
+                    {
+                        TempFile.WriteLine(Add_Before_Line + line + Add_After_Line);
+                        worker_FileProcessor.ReportProgress(progressbar.Value + 1, "Processing File");
+                    }
+                }
+                //Close the temp file. OrgFile running in using statement thus will auto close
+                TempFile.Close();
+            }
+            catch
+            {
+                //If an error occurs during this process we can just abandone the temp file process.
+                //On error we notify the user and they can try again.
+                worker_FileProcessor.ReportProgress(0, "Error While Processing : Abandoning Temp File");
+                if (File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\" + ProcessID + ".tmp")))
+                {
+                    File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\" + ProcessID + ".tmp"));
+                }
+                MessageBox.Show("An Unkown Error Occured While Processing the File.\n Please Try Again","Unable to Process File",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                worker_FileProcessor.ReportProgress(0,"An Error Occured. Please try again.");
+                return;
+            }
+
+            //Once the temp file has been completed, we overwrite the original with the temp file
+            try
+            {
+                worker_FileProcessor.ReportProgress(progressbar.Value + 1,"Finishing Up File");
+                File.Copy(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\" + ProcessID + ".tmp"), TargetFilename, true);
+            }
+            catch
+            {
+                //If an error occurs within this process. We would have to 1. Notify the user and 2. Restore the backup file
+                MessageBox.Show("An error occured while trying to finish your file.\n The backup will be restored if it exists.\nPlease try to process your file again.","Unable to finish the file.",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+                //We try catch any errors with the backup replacement
+                try
+                {
+                    if (File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\" + ProcessID + ".txt")))
+                    {
+                        worker_FileProcessor.ReportProgress(0, "Restoring File Backup");
+                        File.Copy(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\" + ProcessID + ".txt"), TargetFilename, true);
+                        File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\" + ProcessID + ".txt"));
+                    }
+                    MessageBox.Show("No backup file could be Found for the current session.", "No backup file found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                catch
+                {
+                    //If an error occurs within this try catch. There is a clear issue with accessing either of the files. We'll notify the user and request a restart
+                    worker_FileProcessor.ReportProgress(0, "Fatal Error : Please restart the applicaiton and try again.");
+                    MessageBox.Show("The backup file could not be restored. Please restart the application and try again.", "Unable to restore backup file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+                //After restoring backup file, if we can, we exit excution
+                return;
+            }
+
+
+            worker_FileProcessor.ReportProgress(progressbar.Maximum - 1, "Cleaning up our workspace 😊");
+
+            //If everything has been completed wihtout any errors, we remove the left over files
+
+            // Remove the temp file
+            if (File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\" + ProcessID + ".tmp")))
+            {
+                File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\" + ProcessID + ".tmp"));
+            }
+            
+
+            // Remove the backup file
+            if (File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\" + ProcessID + ".txt")))
+            {
+                File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FLE\\Backup\\" + ProcessID + ".txt"));
+            }
+
+            worker_FileProcessor.ReportProgress(progressbar.Maximum, "All Done! Your File is Ready 😁");
+            
+
+
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            TargetFilename = edtTargetFile.Text;
+            Add_Before_Line = edtBeforeLine.Text;
+            Add_After_Line = edtAfterLine.Text;
+
+            //Save current parameters as the defaults
+            SaveDefaults();
+
+            lblProgressMenu.Visible = true;
+            progressbar.Value = 0;
+            worker_FileProcessor.RunWorkerAsync();
+
+    }
+
+        private void worker_FileProcessor_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblProgressMenu.Visible = false;
+            MessageBox.Show("All Done! Your File is Ready 😁", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            //Load Defaults for User Ease
+            int SettingCount = 1;
+            if (File.Exists(Path.Combine(Environment.CurrentDirectory, "default.ini")))
+            {
+                using (StreamReader iniFile = new StreamReader(Path.Combine(Environment.CurrentDirectory, "default.ini")))
+                {
+                    string line;
+
+                    while ((line = iniFile.ReadLine()) != null)
+                    {
+                        switch (SettingCount)
+                        {
+                            case 1:
+                                edtTargetFile.Text = line;
+                                break;
+                            case 2:
+                                edtBeforeLine.Text = line;
+                                break;
+                            case 3:
+                                edtAfterLine.Text = line;
+                                break;
+                            case 4:
+                                if (line == "1")
+                                {
+                                    chckbxMakeBackup.Checked = true;
+                                }
+                                else
+                                {
+                                    chckbxMakeBackup.Checked = false;
+                                };
+                                break;
+                            default:
+                                break;
+                        }
+                        SettingCount += 1;
+                    }
+                }
+            }
+
+           
+            CheckLicense();
+        }
+
+        public void CheckLicense()
+        {
+            //Check if local reg file exists
+            if (File.Exists(LicFileLocation))
+            {
+                //Reg file is present. Check to see if reg is valid.
+                
+                StreamReader LicFile = new StreamReader(LicFileLocation);
+                string RegKey = LicFile.ReadLine();
+                LicFile.Close();
+
+                if (RegKey == null)
+                {
+                    InvokeRegistration();
+                }
+
+                //Check if reg date has arrived
+                if (RegKey[0] == '0')
+                {
+                    //Reg needs to be confirmed. Call REST Server
+                    //Make request to reg server to see if license is valid
+                   
+                    try
+                    {
+                        WebClient RESTRequestor = new WebClient();
+                        string RegURL = "https://example.com/reg.php?key="+ RegKey.Remove(0, 1);
+                        byte[] RawHTML = RESTRequestor.DownloadData(RegURL);
+                        UTF8Encoding UTFEncoder = new UTF8Encoding();
+                        string RegResultString = UTFEncoder.GetString(RawHTML);
+
+                        if (Convert.ToString(RegResultString[RegResultString.LastIndexOf("#") + 1]) == "1") 
+                        {
+                            if (File.Exists(LicFileLocation))
+                            {
+                                File.Delete(LicFileLocation);
+                            }
+                            using (StreamWriter UpdateLicFileDays = new StreamWriter(LicFileLocation))
+                            {
+                                UpdateLicFileDays.WriteLine("5" + RegKey.Remove(0,1));
+                            }
+                            File.SetAttributes(LicFileLocation, FileAttributes.Hidden);
+
+                        }
+                        else
+                        {
+                            //The license key has failed to activate. Remove current license key file for safety
+                            if (File.Exists(LicFileLocation))
+                            {
+                                File.Delete(LicFileLocation);
+                            }
+                            InvokeRegistration();
+                        }
+
+
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Unable to contact the registration server. Please try again later.","Registration Failed",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                    
+                }
+                else
+                {
+                    //Reg is stil valid offline.Close reg process
+                    int NewOfflineDaysLeft = Convert.ToInt32(RegKey[0].ToString()) - 1;
+                    if (File.Exists(LicFileLocation))
+                    {
+                        File.Delete(LicFileLocation);
+                    }
+                    using (StreamWriter UpdateLicFileDays = new StreamWriter(LicFileLocation))
+                    {
+                      UpdateLicFileDays.WriteLine(Convert.ToString(NewOfflineDaysLeft) + RegKey.Remove(0, 1));
+                    }
+                    File.SetAttributes(LicFileLocation, FileAttributes.Hidden);
+                }
+
+               
+
+            }
+            else
+            {
+                //Reg file is missing. A registration is required.
+                InvokeRegistration();
+
+            }
+        }
+
+        private void InvokeRegistration()
+        {
+            
+            DialogResult RegisterResponse = MessageBox.Show("Please Register This Software Before Using It", "Registration Required", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (RegisterResponse == DialogResult.OK)
+            {
+                //Continue with the registration process.
+                frmRegistration RegWindow = new frmRegistration();
+                RegWindow.ShowDialog();
+                RegWindow.Dispose();
+                MessageBox.Show("Please restart this software package to complete the registration", "Restart Required", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+            else
+            {
+                //User rejected the registration. Force kill the application
+                MessageBox.Show("This Software Cannot be Used Without a Valid License.", "Registration Required", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+        }
+    
+        private void SaveDefaults()
+        {
+            if (File.Exists(Path.Combine(Environment.CurrentDirectory, "default.ini")))
+            {
+                File.Delete(Path.Combine(Environment.CurrentDirectory, "default.ini"));
+            }
+
+            using (StreamWriter iniFile = new StreamWriter(Path.Combine(Environment.CurrentDirectory, "default.ini")))
+            {
+                iniFile.WriteLine(edtTargetFile.Text);
+                iniFile.WriteLine(edtBeforeLine.Text);
+                iniFile.WriteLine(edtAfterLine.Text);
+                if (chckbxMakeBackup.Checked)
+                {
+                    iniFile.WriteLine("1");
+                }
+                else
+                {
+                    iniFile.WriteLine("0");
+                }
+            }
+        }
+
+        private void imgLicenseKey_Click(object sender, EventArgs e)
+        {
+            frmRegistration RegWindow = new frmRegistration();
+            RegWindow.ShowDialog();
+            RegWindow.Dispose();
+            MessageBox.Show("Please restart this software package to complete the registration", "Restart Required", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Exit();
+        }
+    }
+}
---
+++ frmRegistration.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace FileLineEditor
+{
+    partial class frmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegistration));
+            this.imgComCenLogo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edtRegSendCode = new System.Windows.Forms.TextBox();
+            this.edtRegReturnCode = new System.Windows.Forms.TextBox();
+            this.btnActivateReg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgComCenLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgComCenLogo
+            // 
+            this.imgComCenLogo.Image = global::FileLineEditor.Properties.Resources.activation;
+            this.imgComCenLogo.Location = new System.Drawing.Point(12, 33);
+            this.imgComCenLogo.Name = "imgComCenLogo";
+            this.imgComCenLogo.Size = new System.Drawing.Size(61, 67);
+            this.imgComCenLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgComCenLogo.TabIndex = 1;
+            this.imgComCenLogo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(174, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serial Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Product Key";
+            // 
+            // edtRegSendCode
+            // 
+            this.edtRegSendCode.Enabled = false;
+            this.edtRegSendCode.Location = new System.Drawing.Point(109, 33);
+            this.edtRegSendCode.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.edtRegSendCode.Name = "edtRegSendCode";
+            this.edtRegSendCode.PasswordChar = '*';
+            this.edtRegSendCode.Size = new System.Drawing.Size(204, 20);
+            this.edtRegSendCode.TabIndex = 4;
+            this.edtRegSendCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // edtRegReturnCode
+            // 
+            this.edtRegReturnCode.Location = new System.Drawing.Point(109, 80);
+            this.edtRegReturnCode.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.edtRegReturnCode.Name = "edtRegReturnCode";
+            this.edtRegReturnCode.Size = new System.Drawing.Size(204, 20);
+            this.edtRegReturnCode.TabIndex = 5;
+            this.edtRegReturnCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnActivateReg
+            // 
+            this.btnActivateReg.CausesValidation = false;
+            this.btnActivateReg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActivateReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActivateReg.Location = new System.Drawing.Point(146, 112);
+            this.btnActivateReg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnActivateReg.Name = "btnActivateReg";
+            this.btnActivateReg.Size = new System.Drawing.Size(125, 25);
+            this.btnActivateReg.TabIndex = 6;
+            this.btnActivateReg.Text = "Register Software";
+            this.btnActivateReg.UseVisualStyleBackColor = true;
+            this.btnActivateReg.Click += new System.EventHandler(this.btnActivateReg_Click);
+            // 
+            // frmRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(356, 165);
+            this.Controls.Add(this.btnActivateReg);
+            this.Controls.Add(this.edtRegReturnCode);
+            this.Controls.Add(this.edtRegSendCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgComCenLogo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmRegistration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Registration";
+            this.Load += new System.EventHandler(this.frmRegistration_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgComCenLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgComCenLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edtRegSendCode;
+        private System.Windows.Forms.TextBox edtRegReturnCode;
+        private System.Windows.Forms.Button btnActivateReg;
+    }
+}
